5eac57ccfb9b330861c96e3ebea67eb752993444
david <2dsant3@gmail.com>
2022-05-27 20:28:40 -0300

    feito novas implementações

--- Services/GeekShopping.ProductApi/Program.cs
+++ Services/GeekShopping.ProductApi/Program.cs
@@ -1,4 +1,7 @@
+using AutoMapper;
+using GeekShopping.ProductApi.Config;
 using GeekShopping.ProductApi.Model.Context;
+using GeekShopping.ProductApi.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +14,12 @@ builder.Services.AddDbContext<MySQLContext>
     mySqlConnectionString,
     ServerVersion.AutoDetect(mySqlConnectionString))
 );
+
+IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
+
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
